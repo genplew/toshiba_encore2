3b3e36d83ae354e3b8cf5a12fcb1e78c619af5e4
genplew <84878478+genplew@users.noreply.github.com>
2022-09-16 10:57:42 -0400

    Add files via upload

---
+++ gestos.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#instalar en linuxmint
+# $ sudo add-apt-repository ppa:touchegg/stable
+# $ sudo apt update
+# $ sudo apt install touchegg
+# configuralo en ~/.config/touchegg/touchegg.conf
+
+touchegg
